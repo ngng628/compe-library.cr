f6be20590419e2bf5184619438404f13fa01bad3
ngng628 <ngng628.kyopro@gmail.com>
2023-05-16 16:26:44 +0900

    :sparkles: 雛形的ファイルを追加

---
+++ src/nglib/utils/utils.cr
@@ -0,0 +1,12 @@
+def int(b = 0); read_line.to_i64 + b end
+def ints(b = 0); read_line.split.map{ |x| x.to_i64 + b } end
+def str; read_line.chomp end
+macro chmax(a, b); ({{a}} < {{b}} && ({{a}} = {{b}})) end
+macro chmin(a, b); ({{a}} > {{b}} && ({{a}} = {{b}})) end
+macro make_array(s, x)
+  Array.new({{ s[0] }}){
+    {% if s[1..s.size].empty? %}; {{ x }}
+    {% else %}; make_array({{ s[1..s.size] }}, {{ x }}) {% end %}
+  }
+end
+require "../constants"
