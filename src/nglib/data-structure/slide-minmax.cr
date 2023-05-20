a8372f258e39dda773a817d6b6c953c993516ab3
ngng628 <ngng628.kyopro@gmail.com>
2023-05-20 20:23:17 +0900

    :recycle: Lint

--- src/nglib/data-structure/slide-minmax.cr
+++ src/nglib/data-structure/slide-minmax.cr
@@ -4,9 +4,9 @@ module NgLib
   # セグメント木やSparseTableと異なり、区間長が固定の範囲でしかクエリに答えられませんが、
   # 計算量が前計算 $O(N)$ で、クエリが $O(1)$ なので高速です。
   #
-  # query(i) で $[i, i + \mathrm{length} - 1})$ が配列の範囲を超えたとき、$[i, \mathrm{a.size})$ だと思って計算します。
+  # `query(i)` で $[i, i + \mathrm{length} - 1)$ が配列の範囲を超えたとき、$[i, \mathrm{a.size})$ だと思って計算します。
   #
-  # もし、query(i) で $[i - \mathrm{length}, i)$ が求まってほしい場合は、`a = ([eins] * length) + a` としておけば良いです。
+  # もし、`query(i)` で $[i - \mathrm{length}, i)$ が求まってほしい場合は、`a = ([eins] * length) + a` としておけば良いです。
   # 範囲外の場合は $[0, i)$ だと思って計算されます。
   #
   # なお、$[0, 0)$ の場合は単位元が返ります。
@@ -16,14 +16,14 @@ module NgLib
   class SlideMinmax(T)
     @length : Int32
     @data : Array(T)
-    
+
     # データ列 $a$ に対して、$\min(a_i, a_{i + 1}, \dots, a_{i + \mathrm{length} - 1})$ を求めるためのデータ構造を構築します。
     #
     # ```
     # rmq = SlideMinmax(Int32).min([2, 7, 3, 4, 6], 3)
     # ```
     def self.min(a : Array(T), length : Int)
-      new(a, length, T::MAX){ |a, b| a <= b }
+      new(a, length, T::MAX) { |lhs, rhs| lhs <= rhs }
     end
 
     # データ列 $a$ に対して、$\max(a_i, a_{i + 1}, \dots, a_{i + \mathrm{length} - 1})$ を求めるためのデータ構造を構築します。
@@ -32,7 +32,7 @@ module NgLib
     # rmq = SlideMinmax(Int32).max([2, 7, 3, 4, 6], 3)
     # ```
     def self.max(a : Array(T), length : Int)
-      new(a, length, T::MIN){ |a, b| a >= b }
+      new(a, length, T::MIN) { |lhs, rhs| lhs >= rhs }
     end
 
     # データ列 $a$ に対して、$cmp(a_i, a_{i + 1}, \dots, a_{i + \mathrm{length} - 1})$ を求めるためのデータ構造を構築します。
