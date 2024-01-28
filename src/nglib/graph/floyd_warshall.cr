8303abc783218a9115f508829014b3b430029ed6
ngng628.kyopro@gmail.com <ngng628.kyopro@gmail.com>
2024-01-28 15:56:30 +0900

    :pencil2: コメントを追加

--- src/nglib/graph/floyd_warshall.cr
+++ src/nglib/graph/floyd_warshall.cr
@@ -1,8 +1,18 @@
 module NgLib
+  # ワーシャル・フロイド法の実装です。
+  #
+  # （負を含む）重み付きグラフに対して、
+  # 全点対最短経路長が $O(V^3)$ で求まります。
   class FloydWarshallGraph(T)
     getter size : Int32
     getter mat : Array(Array(T?))
 
+    # $n$ 頂点 $0$ 辺のグラフを作ります。
+    #
+    # ```
+    # n = 10
+    # NgLib::FloydWarshallGraph(Int64).new(n)
+    # ```
     def initialize(n : Int)
       @size = n.to_i32
       @mat = Array.new(n) { Array.new(n) { nil.as(T?) } }
@@ -11,6 +21,15 @@ module NgLib
       end
     end
 
+    # 隣接行列に従ってグラフを作ります。
+    #
+    # `nil` は辺が存在しないことを表します。
+    # 無限大の重みを持つ辺と捉えても良いです。
+    #
+    # ```
+    # mat = [[0, 3, 1], [-2, 0, 4], [nil, nil, 0]]
+    # NgLib::FloydWarshallGraph(Int32).new(mat)
+    # ```
     def initialize(@mat : Array(Array(T?)))
       @size = @mat.size
       @size.times do |i|
@@ -18,12 +37,44 @@ module NgLib
       end
     end
 
+    # :ditto:
+    def initialize(matrix : Array(Array(T?) | Array(T)))
+      @mat = matrix.map { |line| line.map { |v| v.as(T?) } }
+      @size = @mat.size
+      @size.times do |i|
+        @mat[i][i] = T.zero
+      end
+    end
+
+    # 重みが $w$ の辺 $(u, v)$ を追加します。
+    #
+    # `directed` が `true` である場合、有向辺として追加します。
+    #
+    # ```
+    # n, m = read_line.split.map &.to_i
+    # graph = NgLib::FloydWarshallGraph.new(n)
+    # m.times do
+    #   u, v, w = read_line.split.map &.to_i64
+    #   u -= 1; v -= 1  # 0-index
+    #   graph.add_edge(u, v, w, directed: true)
+    # end
+    # ```
     def add_edge(u : Int, v : Int, w : T, directed : Bool = false)
       @mat[u][v] = w
       @mat[v][u] = w unless directed
     end
 
-    def shortest_path : Array(Array(T?))
+    # 全点対最短経路長を返します。
+    #
+    # どのような経路を辿っても到達できない場合は `nil` が格納されます。
+    #
+    # ```
+    # mat = [[0, 3, 1], [-2, 0, 4], [nil, nil, 0]]
+    # graph = NgLib::FloydWarshallGraph.new(mat)
+    # d = graph.shortest_path # => [[0, 3, 1], [-2, 0, -1], [nil, nil, 0]]
+    # d[0][1] # => 3  (i から j への最短経路長)
+    # ```
+    def shortest_path
       dist = @mat.clone
       @size.times do |via|
         @size.times do |from|
--- src/nglib/graph/tsp.cr
+++ src/nglib/graph/tsp.cr
@@ -1,8 +1,26 @@
 module NgLib
+  # 巡回セールスマン問題を解きます。
+  #
+  # 内部では BitDP を用いているため、
+  # 頂点数が大きいグラフには対応できません。
+  #
+  # 通常の巡回セールスマン問題を解きたい場合は、
+  # `#shortest_route(should_back : Bool = true)` を利用してください。
+  #
+  # 始点を指定したいという特殊な場合は、
+  # `#shortest_route(start : Int, should_back : Bool = true)` を利用してください。
+  #
+  # 計算量は $O(N^2 2^N)$ です。
   class TSPGraph(T)
     getter size : Int32
     getter mat : Array(Array(T?))
 
+    # $n$ 頂点 $0$ 辺のグラフを作ります。
+    #
+    # ```
+    # n = 10
+    # NgLib::TSPGraph(Int64).new(n)
+    # ```
     def initialize(n : Int)
       @size = n.to_i32
       @mat = Array.new(n) { Array.new(n) { nil.as(T?) } }
@@ -11,11 +29,67 @@ module NgLib
       end
     end
 
+    # 隣接行列に従ってグラフを作ります。
+    #
+    # `nil` は辺が存在しないことを表します。
+    # 無限大の重みを持つ辺と捉えても良いです。
+    #
+    # ```
+    # mat = [[0, 3, 1], [-2, 0, 4], [nil, nil, 0]]
+    # NgLib::TSPGraph(Int32).new(mat)
+    # ```
+    def initialize(@mat : Array(Array(T?)))
+      @size = @mat.size
+      @size.times do |i|
+        @mat[i][i] = T.zero
+      end
+    end
+
+    # :ditto:
+    def initialize(matrix : Array(Array(T?) | Array(T)))
+      @mat = matrix.map { |line| line.map { |v| v.as(T?) } }
+      @size = @mat.size
+      @size.times do |i|
+        @mat[i][i] = T.zero
+      end
+    end
+
+    # 重みが $w$ の辺 $(u, v)$ を追加します。
+    #
+    # `directed` が `true` である場合、有向辺として追加します。
+    #
+    # ```
+    # n, m = read_line.split.map &.to_i
+    # graph = NgLib::TSPGraph.new(n)
+    # m.times do
+    #   u, v, w = read_line.split.map &.to_i64
+    #   u -= 1; v -= 1  # 0-index
+    #   graph.add_edge(u, v, w, directed: true)
+    # end
+    # ```
     def add_edge(u : Int, v : Int, w : T, directed : Bool = false)
       @mat[u][v] = w
       @mat[v][u] = w unless directed
     end
 
+    # `dp[S][i] := 今まで訪問した頂点の集合が S で、最後に訪れた頂点が i であるときの最小経路長` を
+    # 返します。
+    #
+    # `should_back` が `true` なら、始点の頂点に戻ってこない場合の最小経路長を計算します。
+    # また、任意の始点に対しての答えを求める点に注意してください。
+    #
+    # `should_back` が `false` なら通常の巡回セールスマン問題の答えです。
+    # 始点が頂点 $0$ であることに注意してください。
+    # つまり、`dp[(1 << n) - 1][0]` が答えです。
+    # 
+    # どのような順でも到達できない場合は `nil` が格納されます。
+    #
+    # ```
+    # graph = TSPGraph(Int64).new(n)
+    # dist = graph.shortest_route
+    # dist[(1 << n) - 1][0] # => ans
+    # dist.last.first # => ans
+    # ```
     def shortest_route(should_back : Bool = true)
       dp = Array.new(1 << @size) { Array.new(@size) { nil.as(T?) } }
 
@@ -27,6 +101,39 @@ module NgLib
         end
       end
 
+      calc(dp)
+    end
+
+    # `dp[S][i] := 今まで訪問した頂点の集合が S で、最後に訪れた頂点が i であるときの最小経路長` を
+    # 返します。
+    #
+    # `should_back` が `true` なら、始点の頂点に戻ってこない場合の最小経路長を計算します。
+    # また、始点が `start` であることに注意してください。
+    #
+    # `should_back` が `false` なら通常の巡回セールスマン問題の答えです。
+    # 始点が頂点 `start` であることに注意してください。
+    # つまり、`dp[(1 << n) - 1][start]` が答えです。
+    # 
+    # どのような順でも到達できない場合は `nil` が格納されます。
+    #
+    # ```
+    # graph = TSPGraph(Int64).new(n)
+    # dist = graph.shortest_route(start: 2)
+    # dist[(1 << n) - 1][2]
+    # ```
+    def shortest_route(start : Int, should_back : Bool = true)
+      dp = Array.new(1 << @size) { Array.new(@size) { nil.as(T?) } }
+
+      if should_back
+        dp[0][start] = T.zero
+      else
+        dp[1 << start][start] = T.zero
+      end
+
+      calc(dp)
+    end
+
+    private def calc(dp : Array(Array(T?)))
       dist = @mat
 
       (1 << @size).times do |visited|
