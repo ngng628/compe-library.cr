a2cc1daca81a14827ca847c6619d88dc3da034db
ngng628 <ngng628.kyopro@gmail.com>
2024-01-28 22:28:20 +0900

    :bug: ミスを修正

--- verify/graph/floyd_warshall.test.cr
+++ verify/graph/floyd_warshall.test.cr
@@ -1,4 +1,5 @@
 # verification-helper: PROBLEM http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_1_C
+
 require "../../src/nglib/graph/floyd_warshall.cr"
 
 n, m = read_line.split.map &.to_i64
@@ -8,7 +9,12 @@ m.times do
   graph.add_edge(u, v, w, directed: true)
 end
 
+
 d = graph.shortest_path
-n.times do |i|
-  puts d[i].map { |elem| elem.nil? ? "INF" : elem }.join ' '
+if n.times.any? { |i| d[i][i] || Int64::MAX < 0 }
+  puts "NEGATIVE CYCLE"
+else
+  n.times do |i|
+    puts d[i].map { |elem| elem.nil? ? "INF" : elem }.join ' '
+  end
 end
