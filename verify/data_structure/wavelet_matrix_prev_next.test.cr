38cc28f9c7f23747578e1f3e49ca62165f299b1a
ngng628.kyopro@gmail.com <ngng628.kyopro@gmail.com>
2024-02-05 00:49:43 +0900

    :white_check_mark: Wavelet Matrix のテストを追加

---
+++ verify/data_structure/wavelet_matrix_prev_next.test.cr
@@ -0,0 +1,25 @@
+# verification-helper: PROBLEM https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=1549
+
+require "../../src/nglib/data_structure/wavelet_matrix"
+
+n = read_line.to_i64
+wm = NgLib::WaveletMatrix.new(read_line.split.map &.to_i64)
+
+q = read_line.to_i64
+q.times do
+  l, r, d = read_line.split.map &.to_i64
+
+  ans = Int64::MAX
+
+  pre = wm.prev_value(l..r, d)
+  unless pre.nil?
+    ans = {ans, (pre - d).abs}.min
+  end
+
+  nxt = wm.next_value(l..r, d)
+  unless nxt.nil?
+    ans = {ans, (nxt - d).abs}.min
+  end
+
+  puts ans
+end
